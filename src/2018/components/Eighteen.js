c9a50f7acb7b9effbf6844883221d79a89543b77
taypsl <tayparsell@gmail.com>
2018-12-29 08:37:46 -0800

    filter mess

--- src/2018/components/Eighteen.js
+++ src/2018/components/Eighteen.js
@@ -18,7 +18,7 @@ const songData2018 = [
 		"description": "Spoon, the most consistently good band currently working, and one of my 10 favorite bands of all time, dropped another gem this year. 'Hot Thoughts' is a swaggering, sinewy jam, perfect for a future James Bond theme song. The sudden bursts of guitar interweave with the jangling bells, swooping strings, and moving bass line to make a nice groove to complement Britt Daniel's passionate yelps.",
 		"imgURL": "http://res.cloudinary.com/ddvn6aemk/image/upload/c_fill,g_north,h_700,w_1300/v1513194248/dbrad2017/large/best_songs_2017_spoon.jpg",
 		"videoUrl": "https://www.youtube.com/watch?v=bKJBSjadBRw",
-		"genres": ["INDIE", "ROCK", "ALL"],
+		"genres": [{"INDIE":true}, {"ROCK":true}],
 		"youtube": true,
 		"start": 32,
 		"end": 71,
@@ -31,7 +31,7 @@ const songData2018 = [
 		"description": "Spoon, the most consistently good band currently working, and one of my 10 favorite bands of all time, dropped another gem this year. 'Hot Thoughts' is a swaggering, sinewy jam, perfect for a future James Bond theme song. The sudden bursts of guitar interweave with the jangling bells, swooping strings, and moving bass line to make a nice groove to complement Britt Daniel's passionate yelps.",
 		"imgURL": "http://res.cloudinary.com/ddvn6aemk/image/upload/c_fill,g_north,h_700,w_1300/v1513194248/dbrad2017/large/best_songs_2017_spoon.jpg",
 		"videoUrl": "https://www.youtube.com/watch?v=mTjQq5rMlEY",
-		"genres": ["POP", "R&amp;B", "ALL"],
+		"genres": [{"POP":true}, {"R&amp;B":true}],
 		"youtube": true,
 		"start": 76,
 		"end": 119,
@@ -44,7 +44,7 @@ const songData2018 = [
 		"description": "Spoon, the most consistently good band currently working, and one of my 10 favorite bands of all time, dropped another gem this year. 'Hot Thoughts' is a swaggering, sinewy jam, perfect for a future James Bond theme song. The sudden bursts of guitar interweave with the jangling bells, swooping strings, and moving bass line to make a nice groove to complement Britt Daniel's passionate yelps.",
 		"imgURL": "http://res.cloudinary.com/ddvn6aemk/image/upload/c_fill,g_north,h_700,w_1300/v1513194248/dbrad2017/large/best_songs_2017_spoon.jpg",
 		"videoUrl": "https://www.youtube.com/watch?v=vquVuj0DWZk",
-		"genres": ["INDIE", "ALL"],
+		"genres": [{"INDIE":true}],
 		"youtube": true,
 		"start": 93,
 		"end": 131,
@@ -57,7 +57,7 @@ const songData2018 = [
 		"description": "Spoon, the most consistently good band currently working, and one of my 10 favorite bands of all time, dropped another gem this year. 'Hot Thoughts' is a swaggering, sinewy jam, perfect for a future James Bond theme song. The sudden bursts of guitar interweave with the jangling bells, swooping strings, and moving bass line to make a nice groove to complement Britt Daniel's passionate yelps.",
 		"imgURL": "http://res.cloudinary.com/ddvn6aemk/image/upload/c_fill,g_north,h_700,w_1300/v1513194248/dbrad2017/large/best_songs_2017_spoon.jpg",
 		"videoUrl": "https://www.youtube.com/watch?v=xTlNMmZKwpA",
-		"genres": ["POP", "R&B", "ALL"],
+		"genres": [{"POP":true}, {"R&B": true}],
 		"youtube": true,
 		"start": 56,
 		"end": 85, //94
@@ -70,7 +70,7 @@ const songData2018 = [
 		"description": "Spoon, the most consistently good band currently working, and one of my 10 favorite bands of all time, dropped another gem this year. 'Hot Thoughts' is a swaggering, sinewy jam, perfect for a future James Bond theme song. The sudden bursts of guitar interweave with the jangling bells, swooping strings, and moving bass line to make a nice groove to complement Britt Daniel's passionate yelps.",
 		"imgURL": "http://res.cloudinary.com/ddvn6aemk/image/upload/c_fill,g_north,h_700,w_1300/v1513194248/dbrad2017/large/best_songs_2017_spoon.jpg",
 		"videoUrl": "https://www.youtube.com/watch?v=B7WiAVLqJOc",
-		"genres": ["INDIE", "ALL"],
+		"genres": [{"INDIE":true}],
 		"youtube": true,
 		"start": 151,
 		"end": 194,
@@ -83,7 +83,8 @@ const songData2018 = [
 		"description": "Spoon, the most consistently good band currently working, and one of my 10 favorite bands of all time, dropped another gem this year. 'Hot Thoughts' is a swaggering, sinewy jam, perfect for a future James Bond theme song. The sudden bursts of guitar interweave with the jangling bells, swooping strings, and moving bass line to make a nice groove to complement Britt Daniel's passionate yelps.",
 		"imgURL": "http://res.cloudinary.com/ddvn6aemk/image/upload/c_fill,g_north,h_700,w_1300/v1513194248/dbrad2017/large/best_songs_2017_spoon.jpg",
 		"videoUrl": "https://www.youtube.com/watch?v=s7tnTucP1UM",
-		"genres": ["INDIE", "ROCK", "ALL"],
+		INDIE: true, 
+		ROCK:true,
 		"youtube": true,
 		"start": 60,
 		"end": 96,
@@ -108,6 +109,10 @@ const menuObj = {
 	]
 }
 
+const GENRE_FILTERS = {
+	"ROCK": song => song.ROCK,
+	"INDIE": song => song.ROCK,
+}
 
 class Eighteen extends Component {
 	constructor(props) {
@@ -206,7 +211,6 @@ class Eighteen extends Component {
 
 					return (
 						<SongCardBlock 
-							
 							song={song} 
 							width={width}
 							id={song._id} 
